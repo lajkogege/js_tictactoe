0990e550abfec19457916c380abc8b063106137e
lajkogege <lajkogege@gmail.com>
2024-04-04 15:20:25 +0200

    xsupa x fuggoleges

--- tesztFugoleges.js
+++ tesztFugoleges.js
@@ -1,12 +1,22 @@
 import {fuggoleges_ell} from "./fuggvenyek.js";
 
 export function tesztFuggolegesEllhez(){
+    nev="üres lista";
     let lista=[" "," "," "," "," "," "," "," "," "];
     let vart="   @   @   @";
-    console.assert(vart===fuggoleges_ell(3,lista), `lista=${lista}, lnko=${vart}`, "A várt eredmény nem stimmel");
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
 
-    lista=[" "," "," "," "," "," "," "," "," "];
-    vart="   @   @   @";
-    console.assert(vart===fuggoleges_ell(3,lista), `lista=${lista}, lnko=${vart}`, "A várt eredmény nem stimmel");
+    nev="Csupa ";
+    lista=["O","O","O","O","O","O","O","O","O"];
+    vart="OOOO@OOO@OOO@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+
+    nev="Csupa X";
+    lista=["X","X","X","X","X","X","X","X","X"];
+    vart="XXX@XYX@XXX@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
 }
 
