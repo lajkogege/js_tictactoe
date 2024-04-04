2e2e4d1b36888099388f60af04acf475a784b718
lajkogege <lajkogege@gmail.com>
2024-04-04 15:27:17 +0200

    javitva let nev valtozo

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { palya_valasztas, jatekKezdese } from "./fuggvenyek.js";
 import teszVizszintesEllenorzes from "./tesztVizszintesEllenorzes.js";
-
+import { tesztFuggolegesEllhez } from "./tesztFugoleges.js";
 window.addEventListener("load", init);
 
 function init() {
   teszVizszintesEllenorzes();
+  tesztFuggolegesEllhez();
   palya_valasztas();
   jatekKezdese();
-  Teszt_atlo();
+  //Teszt_atlo();
 }
 
 
--- tesztFugoleges.js
+++ tesztFugoleges.js
@@ -1,7 +1,7 @@
 import {fuggoleges_ell} from "./fuggvenyek.js";
 
 export function tesztFuggolegesEllhez(){
-    nev="üres lista";
+    let nev="üres lista";
     let lista=[" "," "," "," "," "," "," "," "," "];
     let vart="   @   @   @";
     console.log(nev, lista, vart);
