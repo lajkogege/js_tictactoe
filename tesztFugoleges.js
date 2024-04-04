9ebaf0c565d265bff849260e9848b175300ff0fd
lajkogege <lajkogege@gmail.com>
2024-04-04 15:34:08 +0200

    tesztelési leirás

--- tesztFugoleges.js
+++ tesztFugoleges.js
@@ -18,5 +18,20 @@ export function tesztFuggolegesEllhez(){
     vart="XXX@XYX@XXX@";
     console.log(nev, lista, vart);
     console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
-}
 
+    nev="Csupa X";
+    lista=["X","X","X","X","X","X","X","X","X"];
+    vart="XXX@XYX@XXX@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+}
+/*
+1.üres
+2.csupa X
+3.csupa O
+4.általános nincs győzelem, de nincs minden kitöltve
+5.általános, nincs győzelem, minden ki van töltve
+6.áltlanos nincs győzelem, minden kitöltve
+7. általános van győzelem, minden kivan tötlve 
+8.kukac rosz helyen van
+*/
