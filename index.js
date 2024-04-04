d1fdcbd3188a5fc8cf22e54e95e0bda420d7e89f
leventepatrik <levente.paroczi@gmail.com>
2024-04-04 15:11:14 +0200

    teszt atló function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { palya_valasztas, jatekKezdese } from "./fuggvenyek.js";
+import { Teszt_atlo } from "./teszt_atlo.js";
 
 window.addEventListener("load", init);
 
 function init() {
   palya_valasztas();
   jatekKezdese();
+  Teszt_atlo();
 }
