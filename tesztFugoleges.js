2a5e38e5554270b24b3ca97b36b3f5b33302eff8
lajkogege <lajkogege@gmail.com>
2024-04-10 10:08:00 +0200

    fugoleges teszteles kesz

--- tesztFugoleges.js
+++ tesztFugoleges.js
@@ -1,6 +1,7 @@
 import {fuggoleges_ell} from "./fuggvenyek.js";
 
 export function tesztFuggolegesEllhez(){
+    console.log("Függöleges tesztelés")
     let nev="üres lista";
     let lista=[" "," "," "," "," "," "," "," "," "];
     let vart="   @   @   @";
@@ -15,13 +16,37 @@ export function tesztFuggolegesEllhez(){
 
     nev="Csupa X";
     lista=["X","X","X","X","X","X","X","X","X"];
-    vart="XXX@XYX@XXX@";
+    vart="XXX@XXX@XXX@";
     console.log(nev, lista, vart);
     console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
 
-    nev="Csupa X";
-    lista=["X","X","X","X","X","X","X","X","X"];
-    vart="XXX@XYX@XXX@";
+    nev="általános nincs győzelem, de nincs minden kitöltve";
+    lista=["X","0","X","X","0","X","X","X"," "];
+    vart="XOX@X0X@XX @";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+
+    nev="áltlanos nincs győzelem, minden kitöltve";
+    lista=["X","O","X","O","X","O","O","X","O"];
+    vart="XOX@OXO@OXO@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+
+    nev="áltlanos van győzelem, minden kitöltve";
+    lista=["X","O","X","O","X","O","O","O","X"];
+    vart="XOX@OXO@OOX@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+
+    nev="általános van győzelem, de nincs minden ki tötlve ";
+    lista=["X","O"," "," ","X","O","O"," ","X"];
+    vart="XO @ XO@O X@";
+    console.log(nev, lista, vart);
+    console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
+
+    nev="kukac rosz helyen van ";
+    lista=["X","O","X","O","X","O","O","X","X"];
+    vart="XO@XOXOO@XX@";
     console.log(nev, lista, vart);
     console.assert(vart===fuggoleges_ell(3,lista), "A várt eredmény nem stimmel");
 }
@@ -31,7 +56,7 @@ export function tesztFuggolegesEllhez(){
 3.csupa O
 4.általános nincs győzelem, de nincs minden kitöltve
 5.általános, nincs győzelem, minden ki van töltve
-6.áltlanos nincs győzelem, minden kitöltve
-7. általános van győzelem, minden kivan tötlve 
+6.áltlanos van győzelem, minden kitöltve
+7. általános van győzelem, de nincs minden ki tötlve 
 8.kukac rosz helyen van
 */
